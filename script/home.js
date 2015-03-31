9ea746877b14c4ec11e2384dd2614b6bfce41d4b
Horacio Montes <hmontesgzz@hotmail.com>
2015-03-31 00:03:37 -0600

    nueeva
    x

--- script/home.js
+++ script/home.js
@@ -4,6 +4,10 @@ var estadoBusqueda = document.getElementById("comboEstado").value;
 var salarioBusqueda = document.getElementById("comboSalario").value;
 var diasBusqueda = document.getElementById("comboDias").value;
 
+var nombre = document.getElementById("RegistroNombre").value;
+var apellidos = document.getElementById("RegistroApellidos").value;
+var email = document.getElementById("RegistroEmail").value;
+
 function validarBusquedaHome(){
 	var palabraBusqueda = document.getElementById("buscaPalabra").value;
 	var categoriaBusqueda = document.getElementById("comboCategoria").value;
@@ -16,6 +20,19 @@ function validarBusquedaHome(){
 		salarioBusqueda == 0 && diasBusqueda == 0
 		)
 	{
-		alert("Ingresa al menos un parametro de busqueda");
+		alert("Ingresa al menos un parámetro de busqueda");
+	}
+}
+
+function validarRegistro(){
+	var nombre = document.getElementById("RegistroNombre").value;
+	var apellidos = document.getElementById("RegistroApellidos").value;
+	var email = document.getElementById("RegistroEmail").value;
+	
+	if (
+		nombre == 0 && apellidos == 0 && email == 0
+		)
+	{
+		alert("Ingresa al menos un parámetro de registro");
 	}
 }
